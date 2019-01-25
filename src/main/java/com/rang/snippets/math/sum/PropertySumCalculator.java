85657c552838fb29bf66cb5327e4dd098850c803
GunnarWehlert <gunnar@rang-coding.com>
2019-01-25 01:01:08 +0100

    Added Class PropertySumCalculator

---
+++ src/main/java/com/rang/snippets/math/sum/PropertySumCalculator.java
@@ -0,0 +1,23 @@
+package com.rang.snippets.math.sum;
+
+/**
+ * This class contains multiple examples of how to form a sum over a specific number-property of an object in a
+ * collection.
+ *
+ * @see {@link ArraySumCalculator} for more examples regarding Arrays.
+ * @see {@link CollectionSumCalculator} for more examples regarding Collections.
+ */
+public class PropertySumCalculator {
+
+	/**
+	 * Runner demonstrating usage of above methods.
+	 *
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
+
+		// No examples yet
+		System.out.println("No examples yet");
+	}
+
+}
